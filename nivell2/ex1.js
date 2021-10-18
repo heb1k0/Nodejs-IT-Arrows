502385e1cb5ad3a8dfe324371518da0744210905
Alejandro <alejandrozuriguel@gmail.com>
2021-10-18 22:12:05 +0200

    Add files via upload

---
+++ nivell2/ex1.js
@@ -0,0 +1,9 @@
+// Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut.
+
+let arrowFunction = (param) => {
+    return obj = {
+      value:param
+    }
+  }
+  
+console.log(arrowFunction("test"))
